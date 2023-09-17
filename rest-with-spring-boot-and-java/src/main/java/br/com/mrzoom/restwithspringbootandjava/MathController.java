ce9ec948dffbb1d950decb008c431e4a53cb9574
Igor <igor.nacimento51@gmail.com>
2023-09-17 19:23:17 -0300

    "Defining logic and validation"

--- rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/MathController.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/MathController.java
@@ -13,8 +13,25 @@ public class MathController {
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
-    ){
-        return 1D;
+    ) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return null;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+
+    private boolean isNumeric(String strNumber){
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9+]");
     }
 
 }
