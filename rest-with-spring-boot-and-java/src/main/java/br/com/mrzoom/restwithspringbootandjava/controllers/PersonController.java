70b6810d68ed3d9cf7206aec38cd9f773e3b5c32
Igor <igor.nacimento51@gmail.com>
2023-09-22 14:16:49 -0300

    "Creating method findAll"

--- rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/controllers/PersonController.java
@@ -1,12 +1,12 @@
 package br.com.mrzoom.restwithspringbootandjava.controllers;
 
-import br.com.mrzoom.restwithspringbootandjava.exceptions.UnsupportedMathOperationException;
 import br.com.mrzoom.restwithspringbootandjava.model.Person;
 import br.com.mrzoom.restwithspringbootandjava.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -16,6 +16,11 @@ public class PersonController {
     @Autowired
     private PersonServices service;
     private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return service.findAll();
+    }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) throws Exception{
--- rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/services/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/services/PersonServices.java
@@ -1,9 +1,10 @@
 package br.com.mrzoom.restwithspringbootandjava.services;
 
 import br.com.mrzoom.restwithspringbootandjava.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +13,21 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all people!");
+
+        List<Person> persons = new ArrayList<>();
+        for  (int i = 0; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+
+    }
+
+
 
     public Person findById(String id){
         logger.info("Finding one person!");
@@ -26,4 +42,13 @@ public class PersonServices {
         return person;
     }
 
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Address " + i);
+        person.setGender("Male");
+        return person;
+    }
 }
