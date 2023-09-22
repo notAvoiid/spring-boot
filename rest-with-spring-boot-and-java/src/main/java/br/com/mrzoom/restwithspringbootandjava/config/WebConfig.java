260b9ca4478d9deba86f8c654fd61f09ed2a225f
Igor <igor.nacimento51@gmail.com>
2023-09-22 19:51:49 -0300

    "Content negotiation with Header"

--- rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/config/WebConfig.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/config/WebConfig.java
@@ -10,8 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
-                .parameterName("mediaType").ignoreAcceptHeader(true)
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType").ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                    .mediaType("json", MediaType.APPLICATION_JSON)
+//                    .mediaType("xml", MediaType.APPLICATION_XML);
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                     .mediaType("json", MediaType.APPLICATION_JSON)
