357df59e3a28951597d690056d2edeb25478b744
Igor <igor.nacimento51@gmail.com>
2023-09-23 22:41:40 -0300

    "Creating integration tests to validate CORS Application"

--- rest-with-spring-boot-and-java/src/test/java/br/com/mrzoom/restwithspringbootandjava/unittests/configs/TestConfigs.java
+++ rest-with-spring-boot-and-java/src/test/java/br/com/mrzoom/restwithspringbootandjava/unittests/configs/TestConfigs.java
@@ -4,4 +4,12 @@ public class TestConfigs {
 
     public static final int SERVER_PORT = 8888;
 
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_AVOID = "https://github.com/notAvoiid";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_YML = "application/x-yaml";
+
 }
