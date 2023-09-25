16bbe8dc0d7196e08e7a3d0995cc87bdbb945497
Igor <igor.nacimento51@gmail.com>
2023-09-24 23:07:56 -0300

    "Creating controller integration tests with YAML"

--- rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/data/vo/v1/security/AccountCredentialsVO.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/data/vo/v1/security/AccountCredentialsVO.java
@@ -12,6 +12,8 @@ public class AccountCredentialsVO implements Serializable {
     private String username;
     private String password;
 
+    public AccountCredentialsVO(){}
+
     public AccountCredentialsVO(String username, String password) {
         this.username = username;
         this.password = password;
