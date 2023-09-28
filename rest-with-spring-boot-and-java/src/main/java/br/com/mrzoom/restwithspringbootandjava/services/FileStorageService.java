731f3e30710f6d672ec75d7497309ab8eb7db674
Igor <igor.nacimento51@gmail.com>
2023-09-28 13:10:22 -0300

    "Creating service to storage the archive in the disk"

---
+++ rest-with-spring-boot-and-java/src/main/java/br/com/mrzoom/restwithspringbootandjava/services/FileStorageService.java
@@ -0,0 +1,50 @@
+package br.com.mrzoom.restwithspringbootandjava.services;
+
+import br.com.mrzoom.restwithspringbootandjava.config.FileStorageConfig;
+import br.com.mrzoom.restwithspringbootandjava.exceptions.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e){
+            throw new FileStorageException(
+                    "Could not create the directory where the uploaded files will be stored!", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file){
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        try{
+            if (fileName.contains("..")){
+                throw new FileStorageException(
+                        "Sorry! Filename contains invalid path sequence " + fileName);
+            }
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException(
+                    "Could not store " + fileName + ". Please try again!", e);
+        }
+    }
+}
