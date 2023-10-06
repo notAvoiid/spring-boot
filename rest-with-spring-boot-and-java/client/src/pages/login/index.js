e117b741d656a67ded3bc6f501ee46cf213d5169
Igor <igor.nacimento51@gmail.com>
2023-10-06 16:01:55 -0300

    "Connecting login to the API"

--- rest-with-spring-boot-and-java/client/src/pages/login/index.js
+++ rest-with-spring-boot-and-java/client/src/pages/login/index.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate, Link } from "react-router-dom";
 import './styles.css';
+
+import api from '../../services/api'
 
 import padlock from '../../assets/padlock.png'
 import logoImage from '../../assets/user.png'
 
-export default function Lgoin(){
+export default function Login(){
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  async function login(e) {
+    e.preventDefault();
+
+    const data = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await api.post('auth/signin', data)
+
+      localStorage.setItem('username', username);
+      localStorage.setItem('accessToken', response.data.token);
+
+      navigate('/books')
+    } catch (err) {
+      alert('Login failed! Try again!');
+    }
+  };
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logoImage} alt="User logo"/>
-        <form>
+        <form onSubmit={login}>
           <h1>Access your Account</h1>
-          <input placeholder="Username"/>
-          <input type= "password" placeholder="Password"/>
+          <input
+            placeholder="Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+            
+          <input
+            type= "password" placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
 
           <button className="button" type="submit">Login</button>
         </form>
