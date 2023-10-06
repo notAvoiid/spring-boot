fd0886adec0e777b534e4d3b0bb2a90eb73f7d86
Igor <igor.nacimento51@gmail.com>
2023-10-06 20:03:56 -0300

    "Implementing Delete and Logout"

--- rest-with-spring-boot-and-java/client/src/pages/books/index.js
+++ rest-with-spring-boot-and-java/client/src/pages/books/index.js
@@ -1,78 +1,88 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect} from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import { FiPower, FiEdit, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImage from '../../assets/user.png'
 import './styles.css';
 
 export default function Books(){
+
+  const [books, setBooks] = useState([]);
+
+  const username = localStorage.getItem('username');
+  const accessToken = localStorage.getItem('accessToken');
+
+  const navigate = useNavigate();
+
+  async function logout() {
+    localStorage.clear();
+    navigate('/')
+  }
+
+  async function deleteBook(id) {
+    try {
+      await api.delete(`api/book/v1/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      setBooks(books.filter(book => book.id !== id))
+    } catch (err) {
+      alert('Delete Failed! Try again!')
+    }
+  }
+
+  useEffect(() => {
+    api.get('api/book/v1', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      params: {
+        page: 1,
+        limit: 4,
+        direction: 'asc',
+      }
+    }).then(response => {
+      setBooks(response.data._embedded.bookVOList)
+    })
+  })
+
   return (
     <div className="book-container">
       <header>
         <img src={logoImage} alt="User logo"/>
-        <span>Welcome, <strong>Igor</strong>!</span>
+        <span>Welcome, <strong>{username.toUpperCase()}</strong>!</span>
         <Link className="button" to="/book/new">Add new Book</Link>
-        <button type="button">
+        <button onClick={logout} type="button">
           <FiPower size={18} color="#251FC5" />
         </button>
       </header>
       
       <h1>Registered Books</h1>
       <ul>
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Poulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2020</p>
+
+        {books.map(book => (
+          <li key={book.id}>
+            <strong>Title:</strong>
+            <p>{book.title}</p>
+            <strong>Author:</strong>
+            <p>{book.author}</p>
+            <strong>Price:</strong>
+            <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(book.price)}</p>
+            <strong>Release Date:</strong>
+            <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
           
-          <button type="button">
-            <FiEdit size={20} color="#251FC50"/>
-          </button>
+            <button type="button">
+              <FiEdit size={20} color="#251FC50"/>
+            </button>
           
-          <button type="button">
-            <FiTrash2 size={20} color="#251FC50"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Poulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2020</p>
-          
-          <button type="button">
-            <FiEdit size={20} color="#251FC50"/>
-          </button>
-          
-          <button type="button">
-            <FiTrash2 size={20} color="#251FC50"/>
-          </button>
-        </li>
-        <li>
-          <strong>Title:</strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author:</strong>
-          <p>Nigel Poulton</p>
-          <strong>Price:</strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date:</strong>
-          <p>12/07/2020</p>
-          
-          <button type="button">
-            <FiEdit size={20} color="#251FC50"/>
-          </button>
-          
-          <button type="button">
-            <FiTrash2 size={20} color="#251FC50"/>
-          </button>
-        </li>
+            <button onClick={() => deleteBook(book.id)} type="button">
+              <FiTrash2 size={20} color="#251FC50"/>
+            </button>
+          </li>
+        ))}
       </ul>
 
     </div>
