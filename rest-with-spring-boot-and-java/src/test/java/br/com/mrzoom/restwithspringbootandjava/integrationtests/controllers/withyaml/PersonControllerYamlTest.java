0eb3ae0a0b490bf8aca0c40f2dc430685e0607a5
Igor <igor.nacimento51@gmail.com>
2024-02-29 22:02:14 -0300

    "Changing totalElements"

--- rest-with-spring-boot-and-java/src/test/java/br/com/mrzoom/restwithspringbootandjava/integrationtests/controllers/withyaml/PersonControllerYamlTest.java
+++ rest-with-spring-boot-and-java/src/test/java/br/com/mrzoom/restwithspringbootandjava/integrationtests/controllers/withyaml/PersonControllerYamlTest.java
@@ -442,7 +442,7 @@ class PersonControllerYamlTest extends AbstractIntegrationTest {
 		assertTrue(content.contains("rel: \"next\"  href: \"http://localhost:8888/api/person/v1?direction=asc&page=4&size=10&sort=firstName,asc\""));
 		assertTrue(content.contains("rel: \"last\"  href: \"http://localhost:8888/api/person/v1?direction=asc&page=100&size=10&sort=firstName,asc\""));
 
-		assertTrue(content.contains("page:  size: 10  totalElements: 1007  totalPages: 101  number: 3"));
+		assertTrue(content.contains("page:  size: 10  totalElements: 1008  totalPages: 101  number: 3"));
 	}
 
 	private void mockPerson() {
